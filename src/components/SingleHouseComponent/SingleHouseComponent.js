c35dbbd9508162a39e25a0e5fa68798205abf6f8
Rakelika <raquelsg.gp@gmail.com>
2023-04-04 19:18:26 +0200

    paginador cambios ok

--- src/components/SingleHouseComponent/SingleHouseComponent.js
+++ src/components/SingleHouseComponent/SingleHouseComponent.js
@@ -1,23 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './SingleHouseComponent.scss';
-import { useDispatch, useSelector } from 'react-redux';
-import { getSingleHouse } from '../../store/houses/actions';
+import { useSelector } from 'react-redux';
 
 
 const SingleHouseComponent = () => {
 
-  const {singleHouse,loadingSingleHouse,houses} = useSelector ((state) => state.HousesReducer)
-
-  const dispatch = useDispatch ();
-
-  function handleNext() {
-    dispatch(getSingleHouse(singleHouse.id +1))
-  }
-
-  function handlePrevius() {
-    dispatch(getSingleHouse(singleHouse.id -1))
-  }
+  const {singleHouse,loadingSingleHouse} = useSelector ((state) => state.HousesReducer)
 
   if(loadingSingleHouse){
     return (
@@ -29,14 +18,7 @@ const SingleHouseComponent = () => {
 
 return(
   <div className="SingleHouseComponent">
-    <div>
-      { singleHouse.id > 1 ? (
-      <button onClick={handlePrevius}>Anterior</button>
-      ) : null}
-      { singleHouse.id < 12 ? (
-      <button onClick={handleNext}>Siguiente</button>
-      ) : null}
-    </div>
+
     <div>
       <img src={singleHouse.image} alt={singleHouse.name}/>
       <h2>{singleHouse.name}</h2>
--- src/pages/SingleHousePage/SingleHousePage.js
+++ src/pages/SingleHousePage/SingleHousePage.js
@@ -1,19 +1,34 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
 import { getSingleHouse } from "../../store/houses/actions";
 import SingleHouseComponent from "../../components/SingleHouseComponent/SingleHouseComponent";
 
+
 export default function SingleHousePage() {
 
+    const {singleHouse, houses} = useSelector ((state) => state.HousesReducer)
+
+    const dispatch = useDispatch ();
+
+    const navigate = useNavigate ();
+
     const params = useParams()
-    const dispatch = useDispatch()
+
     useEffect(()=>{
         dispatch(getSingleHouse(params.id))
-    },[])
+    },[params])
 
     return (
         <div>
+            <div>
+                { singleHouse.id > 1 ? (
+                <button onClick={() => navigate (`/house/${singleHouse.id-1}`)}>Previous</button>
+                ) : null}
+                { singleHouse.id < houses.length ?
+                    <button onClick={() => navigate (`/house/${singleHouse.id+1}`)}>Next</button>
+                : null} 
+            </div>
             <SingleHouseComponent></SingleHouseComponent>
         </div>
     )
